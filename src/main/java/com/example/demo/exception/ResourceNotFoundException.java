e88bbf8eb0113ec9259e95541b76abf93d69584b
fjgao <fjgao@bu.edu>
2025-07-08 23:51:44 +0800

    资源不存在返回状态码404

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,9 +1,13 @@
 package com.example.demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author fjgao
  * @date 2025/07/03
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
   public ResourceNotFoundException(String message) {
     super(message);
